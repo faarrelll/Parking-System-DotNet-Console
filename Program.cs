bf6f486c956f00831b12329758b7978b8f20c562
Farrel Akbar <farrelakbar432@gmail.com>
2024-12-24 19:56:32 +0700

    update Entry Point

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ParkingSystem.Commands;
+
+namespace ParkingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var commandHandler = new CommandHandler();
+
+            while (true)
+            {
+                string? command = Console.ReadLine();
+                if (command != null)
+                {
+                    commandHandler.HandleCommand(command);
+                }
+            }
+        }
+    }
+}
